94558b84c3dc586181428d0de0d5dc65f620e449
Noah Dominic <ndsilvio@gmail.com>
2018-06-21 16:37:17 +0800

    Fixed future questions are recent bug

--- polls/views.py
+++ polls/views.py
@@ -2,28 +2,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
-'''
-Code below is from django tutorial part 3. Depricated as of part 4.
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question' : question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question' : question})
-'''
 
 
 class IndexView(generic.ListView):
@@ -32,8 +14,11 @@ class IndexView(generic.ListView):
 
 
     def get_queryset(self):
-        '''Returns the last five published questions'''
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
