df859d6119687690a6634e41829e8d2b71a99303
Noah Dominic <ndsilvio@gmail.com>
2018-06-22 09:30:25 +0800

    Add log out funcitonality

--- sesshandl/urls.py
+++ sesshandl/urls.py
@@ -5,6 +5,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view())
-
+    path('', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
+    #path('/signup', 'registratiion/signup.html', name='signup')
 ]
