2b65681e0cbf5018907eb95b6eb19957ad6b8e17
Noah Dominic <ndsilvio@gmail.com>
2018-06-22 09:07:27 +0800

    Add log in functionality

--- sesshandl/urls.py
+++ sesshandl/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
+from django.contrib.auth import urls
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', auth_views.LoginView.as_view())
+
 ]
